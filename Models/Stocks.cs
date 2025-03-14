39a6d17fd2f8afde248215cfa7fbfd6970ded589
Nabin Kumar Soni <nabinkumar570@gmail.com>
2025-03-15 00:03:41 +0530

    initial commit

---
+++ Models/Stocks.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stocks
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string ComapnyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public string MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
